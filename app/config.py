3206613c78ea58afc70ed77691188b5bcf2b7dc7
Rohit Saundarmal <128357304+Rohiii-777@users.noreply.github.com>
2025-07-10 01:03:09 +0530

    i

--- app/config.py
+++ app/config.py
@@ -7,3 +7,5 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+if DATABASE_URL is None:
+    raise RuntimeError("DATABASE_URL is not set. Please check your environment.")
